17974b5de6193d1a6adeb529eee227a343ba61c5
kyrie <1336861579@qq.com>
2019-08-11 20:48:41 +0800

    init commit

---
+++ src/main/java/com/funtl/itoken/zipkin/ZipKinApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.itoken.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * @author kyrie
+ * @date 2019/8/11 - 20:27
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
